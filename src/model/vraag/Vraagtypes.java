84e88c67eeb9b9b0299c1eb26f4171abcc8d4b47
carl vanderwegen <carl.vanderwegen@student.ucll.be>
2018-07-20 12:37:39 +0200

    enum done

--- src/model/vraag/Vraagtypes.java
+++ src/model/vraag/Vraagtypes.java
@@ -1,4 +1,21 @@
 package model.vraag;
 
 public enum Vraagtypes {
+    JANEE,
+    MultipleChoice;
+
+    public static VraagType GetVraagType(String s){
+        VraagType ret = null;
+        switch (Vraagtypes.valueOf(s.toUpperCase())) {
+            case JANEE:
+                ret =  new JaNee();
+                break;
+            case MultipleChoice:
+                ret =  new MultipleChoice();
+                break;
+            default:
+                break;
+        }
+        return ret;
+    }
 }
